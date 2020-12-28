5ddc84e72d697ad9fa0cf38f86c7031874793ad3
Iago-s <iagooliveira3845@hotmail.com>
2020-12-28 15:25:16 -0300

    Finished

--- src/store/modules/auth/actions.ts
+++ src/store/modules/auth/actions.ts
@@ -4,11 +4,25 @@ export function signInRequest({
   email, 
   password 
 }: { 
-  email: string, 
-  password: string 
+  email: string; 
+  password: string;
 }) {
   return action('@auth/SIGN_IN_REQUEST', { 
     email, 
     password,
   });
+}
+
+export function signInSuccess({ 
+  token
+}: { 
+  token: string;
+}) {
+  return action('@auth/SIGN_IN_SUCCESS', { 
+    token
+  });
+}
+
+export function signInFailure() {
+  return action('@auth/SIGN_IN_FAILURE');
 }
